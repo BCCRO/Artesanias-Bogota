536a189a34f9c01b23d5993216c2f8b5803653c5
StormBlessed27 <hjballent@gmail.com>
2024-12-19 17:36:29 -0500

    Sin cambio alguno

--- artesanias-bogota/src/main/java/com/ud/artesanias_bogota/repositories/RolHasUserRepository.java
+++ artesanias-bogota/src/main/java/com/ud/artesanias_bogota/repositories/RolHasUserRepository.java
@@ -1,7 +1,5 @@
 package com.ud.artesanias_bogota.repositories;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
