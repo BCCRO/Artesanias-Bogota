40b9b190b0695925a24a8bede2eb73a68889a162
JonaCaste <jjcastellanosb@correo.udistrital.edu.co>
2024-12-19 19:36:14 -0500

    todas las rutas habilitadas provisionalmente

--- artesanias-bogota/src/main/java/com/ud/artesanias_bogota/config/SecurityConfig.java
+++ artesanias-bogota/src/main/java/com/ud/artesanias_bogota/config/SecurityConfig.java
@@ -38,13 +38,15 @@ private static final String SECRET_KEY = "8e5a564ea09a55809097ac5fc4fc81e38a1c9c
              * TODO configurarlo mejor, un solo requestMatchers para los endpoints permitidos y luego validar por roles
              */
       authRequest
-        .requestMatchers("/api/productos/productos")
-              .permitAll()
-        .requestMatchers("/api/usuarios/create/cliente")
-              .permitAll()
-        .requestMatchers("/**")
+//        .requestMatchers("/api/productos/productos")
 //              .permitAll()
-              .authenticated()
+//        .requestMatchers("/api/usuarios/create/cliente")
+//              .permitAll()
+//        .requestMatchers("/**")
+////              .permitAll()
+//              .authenticated()
+          .requestMatchers("/**")
+              .permitAll()
 
     )
     .oauth2ResourceServer(oauth2 -> oauth2
