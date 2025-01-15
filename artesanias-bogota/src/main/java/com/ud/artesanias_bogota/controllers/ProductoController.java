6b74b601d6c135f9bb9d019e2d1bcd92a288a9a2
StormBlessed27 <hjballent@gmail.com>
2025-01-15 17:13:20 -0500

    Added update product and changes product state

--- artesanias-bogota/src/main/java/com/ud/artesanias_bogota/controllers/ProductoController.java
+++ artesanias-bogota/src/main/java/com/ud/artesanias_bogota/controllers/ProductoController.java
@@ -10,6 +10,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("/api/productos")
@@ -24,7 +28,7 @@ public class ProductoController {
      */
     @GetMapping(value="/productos", produces = "application/json")
     public ResponseEntity<List<Producto>> getAllProducts(){
-
+      System.out.println("HOLAS");
         List<Producto> productos = productoService.findAllProducto();
 
         if(productos == null || productos.isEmpty()) ResponseEntity.noContent().build();
@@ -63,6 +67,36 @@ public class ProductoController {
         }
 
         return ResponseEntity.ok(producto);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> putMethodName(@PathVariable Long id, @RequestBody ProductoDTO producto) {
+        //TODO: process PUT request
+        try {
+          Producto newProducto = productoService.updateProducto(id, producto);
+          
+          return ResponseEntity.status(202).body(newProducto);
+
+        } catch(IllegalArgumentException e) {
+          return ResponseEntity.status(409).body(e.getLocalizedMessage());
+        }catch (Exception e) {
+          return ResponseEntity.internalServerError().body(null);
+        }
+    }
+
+    @PutMapping(value = "/change_state/{id}/{estado}", produces = "application/json")
+    public ResponseEntity<?> deactivateProduct(@PathVariable Long id, @PathVariable String estado){
+      try {
+        boolean resultado = productoService.deactivate(id,estado);
+        if (!resultado)throw new Exception("Error al actualizar el estado");
+        return ResponseEntity.status(200).body("Estate changed succesfully");
+      } catch(RuntimeException e){
+        return ResponseEntity.status(404).body("Product not found");
+      }catch (Exception e) {
+        return ResponseEntity.status(500).body(e.getMessage());
+      }
+      
+
     }
 
 }
