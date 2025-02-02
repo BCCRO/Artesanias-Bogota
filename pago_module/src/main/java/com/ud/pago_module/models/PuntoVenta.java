46a5ccd0c686ceb26528b436efbb44a38553d28c
StormBlessed27 <hjballent@gmail.com>
2025-02-02 18:46:22 -0500

    Se agregaron los atributos lat y long

--- pago_module/src/main/java/com/ud/pago_module/models/PuntoVenta.java
+++ pago_module/src/main/java/com/ud/pago_module/models/PuntoVenta.java
@@ -207,4 +207,19 @@ public class PuntoVenta {
     public void setCategoriaPuntoVenta(CategoriaPuntoVenta categoriaPuntoVenta) {
         this.categoriaPuntoVenta = categoriaPuntoVenta;
     }
+
+    public Double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(Double longitud) {
+        this.longitud = longitud;
+    }
+
+    public Double getLatitud() {
+        return latitud;
+    }
+    public void setLatitud(Double latitud) {
+        this.latitud = latitud;
+    } 
 }
