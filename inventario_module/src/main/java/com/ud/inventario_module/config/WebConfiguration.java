eef169d4f8883e174e1da3b07aab35fa91aee9f1
JonaCaste <jonacasteb@gmail.com>
2025-02-02 17:01:29 -0500

    change pago module port

--- inventario_module/src/main/java/com/ud/inventario_module/config/WebConfiguration.java
+++ inventario_module/src/main/java/com/ud/inventario_module/config/WebConfiguration.java
@@ -1,4 +1,4 @@
-package com.ud.inventario_module;
+package com.ud.inventario_module.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
