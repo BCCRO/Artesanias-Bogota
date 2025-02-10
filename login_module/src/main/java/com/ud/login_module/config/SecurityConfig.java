e092eed371ce3b8a87e6cbfe842561a59e4ba894
JonaCaste <jonacasteb@gmail.com>
2025-02-09 20:57:56 -0500

    hotfix cors config login 4

--- login_module/src/main/java/com/ud/login_module/config/SecurityConfig.java
+++ login_module/src/main/java/com/ud/login_module/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import javax.crypto.SecretKey;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -43,6 +44,7 @@ public class SecurityConfig {
       .csrf(csrf -> csrf.disable()) // Desactiva la protección contra CSRF.
       .authorizeHttpRequests(authRequest ->
         authRequest
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Permite preflight requests
                 .requestMatchers("/auth/login", "/api/email_auth", "/api/usuarios/create", "/api/usuarios/create/cliente","/api/usuarios/healthcheck")
                     .permitAll()
                 .anyRequest() // Restringe el acceso a todos los endpoints.
